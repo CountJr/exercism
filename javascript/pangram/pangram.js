e7a59f17135c0df25470b8afb57f8e809552603c
Kirill Grafov <countjr@gmail.com>
2017-08-27 17:33:42 +0000

    add pangram second solution

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,13 +1,17 @@
-class pangram {
+class Pangram {
   constructor(text) {
-    this.text = text;
+    this.text = text.toLowerCase();
+    this.charset = 'abcdefghijklmnopqrstuvwxyz';
   }
+
   isPangram() {
-    const re = /([a-z])(?!.*\1)/g;
-    return (this.text.toLowerCase().match(re) || []).length === 26;
+    return ![...this.text].reduce(this.removeChar, this.charset);
+  }
+
+  removeChar(acc,value) {
+    const index = acc.indexOf(value);
+    return index >= 0 ? `${acc.slice(0, index)}${acc.slice(index + 1)}` : acc;
   }
 }
 
-
-
-module.exports = pangram;
+module.exports = Pangram;
---
+++ javascript/pangram/pangram_regex.js
@@ -0,0 +1,11 @@
+class pangram {
+  constructor(text) {
+    this.text = text;
+  }
+  isPangram() {
+    const re = /([a-z])(?!.*\1)/g;
+    return (this.text.toLowerCase().match(re) || []).length === 26;
+  }
+}
+
+module.exports = pangram;
